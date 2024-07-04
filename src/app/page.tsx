4c8ae93bf4742b36ccec13e0e2b922a075324674
vaibhav8788 <vaibhavgupta.v890@gmail.com>
2024-07-05 02:02:43 +0530

    testing git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">home</main>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">This is Hero</main>
   );
 }
